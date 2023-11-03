2f202c4bbfaff52796bf03e7b85ec9a384e3803a
Max Poppe <max.poppe@ugent.be>
2023-11-03 17:33:21 +0100

    refactor

--- AP_GameDev_Project/Entities/Player.cs
+++ AP_GameDev_Project/Entities/Player.cs
@@ -10,6 +10,7 @@ namespace AP_GameDev_Project.Entities
         List<Bullet> bullets;
         private readonly Bullet base_bullet;
         private readonly float bullet_speed;
+        private double bullet_max_cooldown;
         private double bullet_cooldown;
         
         public Player(Vector2 position, Animate stand_animation, float max_speed, Bullet base_bullet, float speed_damping_factor=0.95f): base(position, stand_animation, max_speed, new Rectangle(56, 35, 35, 142), speed_damping_factor)
@@ -17,7 +18,7 @@ namespace AP_GameDev_Project.Entities
             bullets = new List<Bullet>();
             this.base_bullet = base_bullet;
             this.bullet_speed = 10f;
-            this.bullet_cooldown = 0.2f;
+            this.bullet_max_cooldown = 0.2f;
         }
 
         public override void Update(GameTime gameTime)
@@ -46,7 +47,7 @@ namespace AP_GameDev_Project.Entities
                 Vector2 center = new Vector2(hitbox.X + hitbox.Width / 2, hitbox.Y + hitbox.Height / 2);
 
                 bullets.Add(new Bullet(center + new Vector2(angle.X < 0 ? -40: 40, -7), angle * this.bullet_speed, this.base_bullet));
-                this.bullet_cooldown = 0.2f;
+                this.bullet_cooldown = this.bullet_max_cooldown;
             }
         }
     }

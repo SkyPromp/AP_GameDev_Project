d68cb2c0ac2057330c9010f2e1e0eb085ed513c1
SkyPromp <poppe1max@gmail.com>
2023-11-18 11:01:55 +0100

    create hitboxes class

---
+++ AP_GameDev_Project/Entities/Hitboxes.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace AP_GameDev_Project.Entities
+{
+    internal class Hitboxes
+    {
+        private Hitboxes parent;
+        private List<Hitboxes> children;
+
+        private Rectangle hitbox;
+
+        public Hitboxes(Rectangle hitbox, Hitboxes parent=null)
+        {
+            this.hitbox = hitbox;
+            this.parent = parent;
+        }
+
+        public void AddChild(Rectangle child_hitbox)
+        {
+            this.children.Add(new Hitboxes(child_hitbox, this));
+        }
+
+        public void RemoveChild(Hitboxes hitbox)
+        {
+            this.children.Remove(hitbox);
+        }
+
+        public bool DoesCollide(Rectangle other_hitbox)
+        {
+            if(this.children.Count == 0) return this.hitbox.Intersects(other_hitbox);
+
+            return this.children.Any(child_hitbox => child_hitbox.DoesCollide(other_hitbox));
+        }
+    }
+}

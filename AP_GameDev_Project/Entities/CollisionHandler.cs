46b60a2966f86e85d3fdad9e4facadad911070a9
Max Poppe <max.poppe@ugent.be>
2024-01-07 19:32:48 +0100

    cleanup collisionhandler

--- AP_GameDev_Project/Entities/CollisionHandler.cs
+++ AP_GameDev_Project/Entities/CollisionHandler.cs
@@ -9,6 +9,16 @@ using System.Diagnostics;
 using System.Windows.Forms;
 
 
+/*
+ E:  Enemy
+ P:  Player
+ B:  Bullet
+ C:  Collectable
+ T:  Tile
+ Pb: Player bullet
+ Eb: Enemy bullet
+ */
+
 namespace AP_GameDev_Project.Entities
 {
     internal class CollisionHandler
@@ -25,69 +35,30 @@ namespace AP_GameDev_Project.Entities
 
         public List<AEntity> HandleCollision(GameTime gameTime, Player player, List<AEntity> entities, List<ACollectables> collectables, List<Rectangle> tile_hitboxes, MouseHandler mouseHandler)
         {
-            /*
-             E:  Enemy
-             P:  Player
-             B:  Bullet
-             C:  Collectable
-             T:  Tile
-             Pb: Player bullet
-             Eb: Enemy bullet
-             */
-
-            Vector2 player_center = player.GetCenter;
-            bool is_player = true;
-
-
-
             foreach (AEntity entity in new List<AEntity>(entities))  // One big foreach, for performance reasons
             {
-                entity.Update(gameTime, is_player ? mouseHandler.MousePos : player_center);
-                List<Bullet> entity_bullets = new List<Bullet>(entity.Bullets);
-
-                entities = EECollision(entity, entities);
+                EECollision(entity, entities);
                 ETBCollision(entity, tile_hitboxes);
 
-                if (is_player)
-                {
-                    is_player = false;
-                    continue;
-                }
+                if (entity is Player) continue;
 
-                if (PbECollision(entity, player))
-                {
-                    entity.Die(this.contentManager);
-                    entities.Remove(entity);
-                }
-
+                PbECollision(entity, player);
                 EbPCollision(entity, player);
             }
 
             PCCollision(player, collectables);
 
-            if (entities.Count == 1)
-            {
-                this.stateHandler.SetCurrentState(StateHandler.states_enum.END).Init();
-                ((EndStateHandler)this.stateHandler.States[StateHandler.states_enum.END]).Won = true;
-            }
-
             return entities;
         }
 
-        private List<AEntity> EECollision(AEntity entity, List<AEntity> entities)
+        private void EECollision(AEntity entity, List<AEntity> entities)
         {
             List<AEntity> entities_list = new List<AEntity>(entities);
             foreach (AEntity entity2 in entities)
             {
                 if ((entity2.GetHitbox.Center - entity.GetHitbox.Center).ToVector2().Length() > 1)
-                {
-                    bool kill_entity2 = this.hitboxCollisionHelper.HandleHardCollison(entity, entity2); // average movement out
-
-                    if (kill_entity2) entities_list.Remove(entity2);
-                }     
+                    this.hitboxCollisionHelper.HandleHardCollison(entity, entity2); // average movement out  
             }
-
-            return entities_list;
         }
 
         private void ETBCollision(AEntity entity, List<Rectangle> tile_hitboxes)
@@ -106,25 +77,21 @@ namespace AP_GameDev_Project.Entities
             entity.Bullets = entity_bullets;
         }
 
-        private bool PbECollision(AEntity entity, Player player)
+        private void PbECollision(AEntity entity, Player player)
         {
             List<Bullet> player_bullets = new List<Bullet>(player.Bullets);
-            bool remove_entity = false;
             Hitbox hitbox = entity.GetHitboxHitbox;
 
             foreach (Bullet bullet in player.Bullets)
             {
                 if (!bullet.GetHitboxHitbox.DoesCollideR(hitbox).Item1.IsEmpty)
                 {
-                    int health = entity.DoDamage(bullet.Damage);
+                    entity.DoDamage(bullet.Damage);
                     player_bullets.Remove(bullet);
-                    remove_entity = health <= 0;
                 }
             }
 
             player.Bullets = player_bullets;
-
-            return remove_entity;
         }
 
         private void EbPCollision(AEntity entity, Player player)
@@ -136,15 +103,8 @@ namespace AP_GameDev_Project.Entities
             {
                 if (!bullet.GetHitboxHitbox.DoesCollideR(hitbox).Item1.IsEmpty)
                 {
-                    int health = player.DoDamage(bullet.Damage);
+                    player.DoDamage(bullet.Damage);
                     entity_bullets.Remove(bullet);
-                    if (health <= 0)
-                    {
-                        this.contentManager.GetSoundEffects["PLAYER_DEATH"].Play();
-                        
-                        this.stateHandler.SetCurrentState(StateHandler.states_enum.END).Init();
-                        ((EndStateHandler) this.stateHandler.States[StateHandler.states_enum.END]).Won = false;
-                    }
                 }
             }
 
